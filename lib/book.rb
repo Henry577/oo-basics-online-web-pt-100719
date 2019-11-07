ad7b6d2d8562a3085cf3c0901aef14682f4be3ef
Henry Allan <henryallan50@gmail.com>
2019-11-07 22:07:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,10 @@
 class Book
   
+  attr_accessor :title, :author, :page_count, :genre, :turn_page
+  
   def initialize(title = "none")
     
     @title = title
-    
-  end
-  
-  def title
-    
-    @title
     
   end
   
